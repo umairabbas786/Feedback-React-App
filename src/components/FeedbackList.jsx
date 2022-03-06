2819ae3c414aab46e1c3f1cf8e711762dccd9408
Umair Abbas <umairabbass786@gmail.com>
2022-03-06 21:20:01 +0500

    first commit (feedback created)

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,16 @@
+import FeedbackItem from "./FeedbackItem"
+
+function FeedbackList({ feedback }) {
+    if(!feedback || feedback.length === 0) {
+        return <p>No Feedback Yet</p>
+    }
+  return (
+    <div className="feedback-lsit">
+        {feedback.map((item) => (
+            <FeedbackItem key={item.id} item={item}/>
+        ))}
+    </div>
+  )
+}
+
+export default FeedbackList
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit',
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Umair Abbas is a dgood boy',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Hashim is a bad boy',
+    }
+]
+
+export default FeedbackData
